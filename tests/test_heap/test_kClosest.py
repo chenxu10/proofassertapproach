6a6b4222f96be038633ad82a4a352bb46f85e6a9
chenxu10 <shenxu2011@gmail.com>
2025-08-15 11:33:53 -0400

    [S]iterate on a much simpleer version of change

--- tests/test_heap/test_kClosest.py
+++ tests/test_heap/test_kClosest.py
@@ -18,8 +18,7 @@ def kClosest(points: List[List[int]], k: int) -> List[List[int]]:
 
     heap = []
 
-    for _, pos in enumerate(points):
-        x, y = pos
+    for x,y in points:
         distance = x ** 2 + y ** 2
         heapq.heappush(heap, (distance, (x,y)))
 
