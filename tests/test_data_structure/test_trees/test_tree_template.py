7db7e7335737d644ffe09b2e6647de106a26cac1
chenxu10 <shenxu2011@gmail.com>
2025-08-20 13:03:27 -0400

    [B]finish consolidation four easy problems of tree invert, depth, same, subree of another tree

---
+++ tests/test_data_structure/test_trees/test_tree_template.py
@@ -0,0 +1,6 @@
+def tree_template(root):
+    # handle root is None
+    # handle a single root
+    # assume required for root.left and root.right solved
+    # final return handle relationship of left return and right return
+    pass
